e36cf79b8aafcbf55d3ccdaba751b2f7758b6711
Nandhinivollala <nandinivollala356@gmail.com>
2026-07-06 01:38:04 +0530

    fix: support Vercel Blob OIDC

--- src/modules/files/object-storage.ts
+++ src/modules/files/object-storage.ts
@@ -3,6 +3,7 @@ import "server-only";
 import { localObjectStorage } from "./local-storage";
 import { vercelBlobObjectStorage } from "./vercel-blob-storage";
 
-export const objectStorage = process.env.BLOB_READ_WRITE_TOKEN
+export const objectStorage =
+  process.env.BLOB_STORE_ID || process.env.BLOB_READ_WRITE_TOKEN
   ? vercelBlobObjectStorage
   : localObjectStorage;
